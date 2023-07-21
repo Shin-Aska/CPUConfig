e6895a7d3948ef89229ccfccbfd25cd0d7825d94
Richard Orilla <Orillarichard@pm.me>
2023-07-22 01:40:59 +0800

    Add core id and temperature on CoreInfo class

--- coreinfo.cpp
+++ coreinfo.cpp
@@ -1,12 +1,14 @@
 #include "coreinfo.h"
 
-CoreInfo::CoreInfo(int pid, std::string vendor, int cpuFamilyId, int modelId, std::string modelName)
+CoreInfo::CoreInfo(int pid, int cid, std::string vendor, int cpuFamilyId, int modelId, std::string modelName, double coreTemperature)
 {
     this->processorId = pid;
+    this->coreId = cid;
     this->vendorId = vendor;
     this->cpuFamilyId = cpuFamilyId;
     this->modelId = modelId;
     this->modelName = modelName;
+    this->temperature = coreTemperature;
 }
 
 CoreInfo::CoreInfo()
--- coreinfo.h
+++ coreinfo.h
@@ -6,13 +6,15 @@
 class CoreInfo
 {
 public:
-    CoreInfo(int, std::string, int, int, std::string);
+    CoreInfo(int, int, std::string, int, int, std::string, double);
     CoreInfo();
     int processorId;
+    int coreId;
     std::string vendorId;
     int cpuFamilyId;
     int modelId;
     std::string modelName;
+    double temperature;
 };
 
 #endif // COREINFO_H
